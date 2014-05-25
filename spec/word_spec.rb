ac3ae50811de3953104dae77d81532a2a0bc5f91
Candace Schenk <candace.schenk@gmail.com>
2014-05-24 21:54:49 -0400

    starts test outline for words

---
+++ spec/word_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require 'letter'
+require 'word'
+
+describe Word do
+  
+  describe "#to_s" do
+    
+  end
+  
+  describe "#to_latex" do
+    
+  end
+  
+  describe "#[]" do
+    
+  end
+  
+  describe "#[]=" do
+  end
+  
+  describe "#each" do
+  end
+  
+  describe "#length" do
+  end
+  
+  describe "#number_of_letters" do
+  end
+  
+  describe "#eql?" do
+    it "return true when the letters' order is the same"
+  end
+  
+  describe "#==" do
+    it "return true when the group elements are equal"
+  end
+end
